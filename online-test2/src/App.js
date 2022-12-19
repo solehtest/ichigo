8b2102e470e66589dae6d071219abdeac798ee9a
solehMQ <solehudinmq@gmail.com>
2022-12-19 19:38:02 +0700

    ui fullscreen

--- online-test2/src/App.js
+++ online-test2/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useMediaQuery } from 'react-responsive'
 
 function App() {
+  const isFullscreen = useMediaQuery({
+    query: '(min-width: 601px)'
+  })
+
+  const isMobile = useMediaQuery({
+    query: '(max-width: 600px)'
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="grid-container">
+    //   <div className="item1">Header</div>
+    //   <div className="item2">Menu</div>
+    //   <div className="item3">Main</div>  
+    //   <div className="item4">Right</div>
+    //   <div className="item5">Footer</div>
+    // </div>
+
+    <div className="grid-container">
+      <div className="item1">1</div>
+      <div className="item2">2</div>  
+      <div className="item3">3</div>
+      <div className="item4">4</div>
+      <div className="item5">5</div>
+      <div className="item6">6</div>
+      <div className="item7">7</div>
+      <div className="item8">8</div>
+      <div className="item9">9</div>
     </div>
+
+    // <div className="grid-container">
+    //   <div className="item1">1</div>
+    //   <div className="item2">2</div>
+    //   <div className="item3">3</div>  
+    //   <div className="item4">4</div>
+    //   <div className="item5">5</div>
+    //   <div className="item6">6</div>
+    //   <div className="item7">7</div>
+    //   <div className="item8">8</div>
+    //   <div className="item9">9</div>
+    // </div>
   );
 }
 
