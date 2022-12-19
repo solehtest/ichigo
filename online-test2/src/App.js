20f3b82ff96324e2f5e6a88357474b772baf6bb8
solehMQ <solehudinmq@gmail.com>
2022-12-19 20:14:32 +0700

    ui mobile

--- online-test2/src/App.js
+++ online-test2/src/App.js
@@ -1,48 +1,37 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useMediaQuery } from 'react-responsive'
+import MediaQuery from 'react-responsive'
 
 function App() {
-  const isFullscreen = useMediaQuery({
-    query: '(min-width: 601px)'
-  })
-
-  const isMobile = useMediaQuery({
-    query: '(max-width: 600px)'
-  })
-
   return (
-    // <div className="grid-container">
-    //   <div className="item1">Header</div>
-    //   <div className="item2">Menu</div>
-    //   <div className="item3">Main</div>  
-    //   <div className="item4">Right</div>
-    //   <div className="item5">Footer</div>
-    // </div>
-
-    <div className="grid-container">
-      <div className="item1">1</div>
-      <div className="item2">2</div>  
-      <div className="item3">3</div>
-      <div className="item4">4</div>
-      <div className="item5">5</div>
-      <div className="item6">6</div>
-      <div className="item7">7</div>
-      <div className="item8">8</div>
-      <div className="item9">9</div>
+    <div>
+      <MediaQuery minWidth={601}>
+        <div className="grid-container">
+          <div className="item1">1</div>
+          <div className="item2">2</div>  
+          <div className="item3">3</div>
+          <div className="item4">4</div>
+          <div className="item5">5</div>
+          <div className="item6">6</div>
+          <div className="item7">7</div>
+          <div className="item8">8</div>
+          <div className="item9">9</div>
+        </div>
+      </MediaQuery>
+      <MediaQuery maxWidth={600}>
+        <div className="grid-container-small">
+          <div className="item-small1">1</div>
+          <div className="item-small3">3</div>  
+          <div className="item-small4">4</div>
+          <div className="item-small2">2</div>
+          <div className="item-small7">7</div>
+          <div className="item-small5">5</div>
+          <div className="item-small8">8</div>
+          <div className="item-small6">6</div>
+          <div className="item-small9">9</div>
+        </div>
+      </MediaQuery>
     </div>
-
-    // <div className="grid-container">
-    //   <div className="item1">1</div>
-    //   <div className="item2">2</div>
-    //   <div className="item3">3</div>  
-    //   <div className="item4">4</div>
-    //   <div className="item5">5</div>
-    //   <div className="item6">6</div>
-    //   <div className="item7">7</div>
-    //   <div className="item8">8</div>
-    //   <div className="item9">9</div>
-    // </div>
   );
 }
 
